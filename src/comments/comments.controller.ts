cf7b0f24eb88e65480618b747aa546ed38fe38ed
nbintang <nbintangh@gmail.com>
2025-05-19 02:25:06 +0700

    feat(comments): set CRUD controller for commetns

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -6,36 +6,70 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentDto } from './dtos/mutate-comment.dto';
+import { SinglePayloadResponseDto } from 'src/common/dtos/single-payload-response.dto';
+import { PaginatedPayloadResponseDto } from 'src/common/dtos/paginated-payload-response.dto';
+import { QueryCommentDto } from './dtos/query-comment.dto';
 
-@Controller('comments')
+@Controller('/protected/comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Post()
-  create(@Body() createCommentDto: CommentDto) {
-    return this.commentsService.createComment(createCommentDto);
+  async createComment(
+    @Body() createCommentDto: CommentDto,
+  ): Promise<SinglePayloadResponseDto> {
+    const newComment =
+      await this.commentsService.createComment(createCommentDto);
+    return {
+      data: newComment,
+    };
   }
 
   @Get()
-  findAll() {
-    return this.commentsService.getAllComments();
+  async getAllComments(
+    @Query() query: QueryCommentDto,
+  ): Promise<PaginatedPayloadResponseDto> {
+    const { comments, meta } = await this.commentsService.getAllComments(query);
+    return {
+      data: {
+        items: comments,
+        meta,
+      },
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentsService.getCommentById(+id);
+  async getCommentById(
+    @Param('id') id: string,
+  ): Promise<SinglePayloadResponseDto> {
+    const comment = await this.commentsService.getCommentById(id);
+    return {
+      data: comment,
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: CommentDto) {
-    return this.commentsService.updateCommentById(+id, updateCommentDto);
+  async updateCommentById(
+    @Param('id') id: string,
+    @Body() updateCommentDto: CommentDto,
+  ): Promise<SinglePayloadResponseDto> {
+    const comment = await this.commentsService.updateCommentById(
+      id,
+      updateCommentDto,
+    );
+    return {
+      data: comment,
+    };
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentsService.removeCommentById(+id);
+  async removeCommentById(
+    @Param('id') id: string,
+  ): Promise<SinglePayloadResponseDto> {
+    return this.commentsService.removeCommentById(id);
   }
 }
