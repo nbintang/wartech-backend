7099acf039f26a35bfe36f3e84d13e139950defa
nbintang <nbintangh@gmail.com>
2025-05-24 00:13:00 +0700

    fix(prisma): fixing prisma import issues

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @SkipThrottle({ short: true, medium: true, long: true })
   @Get()
   getHello(): string {
     return this.appService.getHello();
