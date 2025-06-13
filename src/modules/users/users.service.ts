4b54701b2017acbd8dc7a73943c2b0f7a05cd32a
bintang <119093997+nbintang@users.noreply.github.com>
2025-06-13 17:14:35 +0700

    Update users.service.ts

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -10,7 +10,7 @@ type UserPayload<T extends Prisma.UserDefaultArgs = object> =
   Prisma.UserGetPayload<T>;
 
 type SafeUser = UserPayload<{
-  omit: { password: true; acceptedTOS: true; emailVerifiedAt: true };
+  omit: { password: true; acceptedTOS: true; emailVerifiedAt: true; resendEmailCooldown: true };
 }>;
 
 @Injectable()
