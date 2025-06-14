9e74b1c25ab090f3992bbda0908bccbb8814612f
bintang <119093997+nbintang@users.noreply.github.com>
2025-06-14 17:13:33 +0700

    Update mutate-user.dto.ts

--- src/modules/users/dtos/mutate-user.dto.ts
+++ src/modules/users/dtos/mutate-user.dto.ts
@@ -2,9 +2,11 @@ import { createZodDto } from 'nestjs-zod';
 import { baseUserSchema } from './user.schema';
 
 export const createUserSchema = baseUserSchema;
-export const updateUserSchema = baseUserSchema.omit({
-  acceptedTOS: true,
-  password: true,
-});
+export const updateUserSchema = baseUserSchema
+  .omit({ acceptedTOS: true, password: true })
+  .extend({ 
+    name: baseUserSchema.shape.name.optional(), 
+    email: baseUserSchema.shape.email.optional()
+  });
 export class UpdateUserDto extends createZodDto(updateUserSchema) {}
 export class CreateUserDto extends createZodDto(createUserSchema) {}
