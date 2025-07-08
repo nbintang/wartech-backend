d7214c5b2fe0fa9de2cb6b41a6e1281ea50c2949
nbintang <nbintangh@gmail.com>
2025-07-08 20:32:41 +0700

    fix: bootstrap port env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import cookieParser from 'cookie-parser';
+
 import { HttpExceptionFilter } from './commons/http-exception/http-exception.filter';
 import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import compression from 'compression';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const allowedOrigins = [
+    'http://localhost:3000',
+    configService.get<string>('FRONTEND_URL'),
+  ];
   app.use(cookieParser());
   app.useGlobalFilters(new HttpExceptionFilter(new Logger()));
   app.setGlobalPrefix('api');
@@ -14,7 +21,7 @@ async function bootstrap() {
   app.useBodyParser('urlencoded', { extended: true });
   app.use(compression());
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
+    origin: allowedOrigins,
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
