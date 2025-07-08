6807e9200d3d850db16fc9d821328ee99930685d
nbintang <nbintang@gmail.com>
2025-07-08 14:09:06 +0700

    fix(article): adding with artticle query

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -54,31 +54,21 @@ export class CategoriesService {
     const dynamicSearch: Prisma.CategoryWhereInput = {
       ...(query.name && { name: { contains: query.name } }),
     };
+   const include = query['with-articles']
+  ? {
+      articles: {
+        select: { id: true, title: true, slug: true, image: true, status: true, publishedAt: true },
+        where: { status: ArticleStatus.PUBLISHED },
+        orderBy: { publishedAt: 'desc' },
+        ...(query['articles-per-category'] && { take: query['articles-per-category'] }),
+      },
+    }
+  : undefined;
     const categories = await this.db.category.findMany({
       where: dynamicSearch,
       skip,
       take,
-      include: {
-        articles: {
-          select: {
-            id: true,
-            title: true,
-            slug: true,
-            image: true,
-            status: true,
-            publishedAt: true,
-          },
-          where: {
-            status: ArticleStatus.PUBLISHED,
-          },
-          orderBy: {
-            publishedAt: 'desc',
-          },
-          ...(query['articles-per-category'] && {
-            take: query['articles-per-category'],
-          }),
-        },
-      },
+      include,
     });
     const categoriesCount = await this.db.category.count({
       where: dynamicSearch,
--- src/modules/categories/dtos/query-categories.dto.ts
+++ src/modules/categories/dtos/query-categories.dto.ts
@@ -6,6 +6,7 @@ const queryCategorisSchema = baseQueryRequestSchema.extend({
   name: z.string().optional(),
   slug: z.string().optional(),
   'articles-per-category': z.coerce.number().optional(),
+  'with-articles': z.coerce.boolean().optional().default(true), // default true kalau mau
 });
 
 export class QueryCategoriesDto extends createZodDto(queryCategorisSchema) {}
