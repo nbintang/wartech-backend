3921ec2912c8887ca3b0736da99cfd6159dd9cb5
nbintang <nbintangh@gmail.com>
2025-07-08 20:39:42 +0700

    fix: boostrap port env

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import cookieParser from 'cookie-parser';
-
 import { HttpExceptionFilter } from './commons/http-exception/http-exception.filter';
 import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import compression from 'compression';
-import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
-  const allowedOrigins = [
-    'http://localhost:3000',
-    configService.get<string>('FRONTEND_URL'),
-  ];
   app.use(cookieParser());
   app.useGlobalFilters(new HttpExceptionFilter(new Logger()));
   app.setGlobalPrefix('api');
@@ -21,7 +14,7 @@ async function bootstrap() {
   app.useBodyParser('urlencoded', { extended: true });
   app.use(compression());
   app.enableCors({
-    origin: allowedOrigins,
+    origin: ['http://localhost:3000', 'https://wartech-frontend.vercel.app'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
